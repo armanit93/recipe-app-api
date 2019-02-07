b34f688a8f33a971721cb9cc7d183101a032b6d6
arman <armanit93@gmail.com>
2019-02-07 12:13:35 -0500

    tdd started

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from app.calc import add
+from app.calc import add, subtract
 
 
 
@@ -8,5 +8,10 @@ class CalcTests(TestCase):
     def test_add_number(self):
         """Testign Add functionality in Calc"""
         self.assertEqual(add(3,8),11)
+
+
+    def test_subtract_number(self):
+    	"""Testing Subtract Functionality in Calc """
+    	self.assertEqual(subtract(8,3),5)
 
 
